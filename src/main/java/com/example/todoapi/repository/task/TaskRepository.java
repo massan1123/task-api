99d7f09ad2493666a4b425e7363a3b45fc0e3b92
m-fujimoto <fujimoto0922jp@gmail.com>
2023-10-11 17:34:14 +0900

    新規作成

---
+++ src/main/java/com/example/todoapi/repository/task/TaskRepository.java
@@ -0,0 +1,25 @@
+package com.example.todoapi.repository.task;
+
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface TaskRepository {
+    @Select("SELECT id, title FROM tasks WHERE id = #{taskId}")
+    Optional<TaskRecord> select(Long taskId);
+
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    @Insert("INSERT INTO tasks (title) VALUES (#{title})")
+    void insert(TaskRecord record);
+
+    @Select("SELECT id, title FROM tasks LIMIT #{limit} OFFSET #{offset}")
+    List<TaskRecord> selectList(int limit, long offset);
+
+    @Update("UPDATE tasks SET title = #{title} WHERE id = #{id}")
+    void update(TaskRecord taskRecord);
+
+    @Delete("DELETE FROM tasks WHERE id = #{id}")
+    void delete(Long taskId);
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,7 @@
 INSERT INTO samples (content) values ('Hello, world!!');
+
+INSERT INTO tasks (title) values ('タスク１');
+INSERT INTO tasks (title) values ('タスク２');
+INSERT INTO tasks (title) values ('タスク３');
+INSERT INTO tasks (title) values ('タスク４');
+INSERT INTO tasks (title) values ('タスク５');
